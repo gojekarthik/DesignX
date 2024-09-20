8a3ed24128a0e5cb1a03768aa3f8dcd7840eb4bc
gojekarthik <karthikgoje99@gmail.com>
2024-09-20 20:44:19 +0530

    added navbar with active users

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "liveblocks.io",
+        pathname: "/avatars/**", // This allows all images from this specific path.
+      },
+    ]
+  },
+};
 
-export default nextConfig;
+export default nextConfig
